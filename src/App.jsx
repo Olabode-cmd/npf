4db2bcc10c6a265b9a3f65b8a1df41d9933c2cb4
Olabode-cmd <olabodebalogun80@gmail.com>
2025-05-09 09:32:16 +0100

    redirect to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/auth/login')
+  }, [])
 
   return (
     <>
